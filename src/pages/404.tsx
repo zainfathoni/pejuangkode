069498ac3455928c4b53ee8f895a78809075fd4f
Zain Fathoni <zain.fathoni@gmail.com>
2019-12-14 19:45:30 +0800

    Fix ESLint warnings & errors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Layout from "@narative/gatsby-theme-novela/src/components/Layout";
 import Section from "@narative/gatsby-theme-novela/src/components/Section";
 import SEO from "@narative/gatsby-theme-novela/src/components/SEO";
