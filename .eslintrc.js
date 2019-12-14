ddc70840d0963e0e8ce500f28aa1900c5ea59945
Zain Fathoni <zain.fathoni@gmail.com>
2019-12-14 20:16:27 +0800

    Setup Prettier to work along with ESLint
    https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   plugins: ["react-hooks"],
   parserOptions: {
